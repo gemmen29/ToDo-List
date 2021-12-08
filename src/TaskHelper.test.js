f1a6fb48a08f3317cd2057b0f7a3cbaf2e534dcd
George Magdy <georgtriple@gmail.com>
2021-12-08 21:50:49 +0200

    Add remove task tests

--- src/TaskHelper.test.js
+++ src/TaskHelper.test.js
@@ -13,3 +13,15 @@ describe("add new test", () => {
     expect(TaskHelper.tasksList).toHaveLength(0);
   });
 });
+
+describe("remove task", () => {
+  test("remove task", () => {
+    const task1 = new TaskHelper(0, "New task");
+    const task2 = new TaskHelper(1, "New task");
+    TaskHelper.addNew(task1);
+    TaskHelper.addNew(task2);
+    TaskHelper.remove(1);
+    expect(TaskHelper.tasksList).toHaveLength(1);
+    TaskHelper.tasksList.pop();
+  });
+})
