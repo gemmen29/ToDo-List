381b4e5aba614b057f0e3976a030e797fceb0266
George Magdy <georgtriple@gmail.com>
2021-12-08 22:59:21 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,61 +1,58 @@
-import "./style.css";
+import './style.css';
 
-import LocalStorageHelper from "./LocalStorageHelper";
-import TaskHelper from "./TaskHelper";
+import LocalStorageHelper from './LocalStorageHelper';
+import TaskHelper from './TaskHelper';
 
-const tasksKeyInLocalStorage = "tasks-list";
+const tasksKeyInLocalStorage = 'tasks-list';
 export default class LoadContent {
   static createLiForTask(task) {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
 
-    const containerDivForCheckText = document.createElement("div");
-    containerDivForCheckText.classList.add("d-flex", "center");
+    const containerDivForCheckText = document.createElement('div');
+    containerDivForCheckText.classList.add('d-flex', 'center');
 
-    const liCheckBox = document.createElement("input");
-    liCheckBox.type = "checkbox";
+    const liCheckBox = document.createElement('input');
+    liCheckBox.type = 'checkbox';
     liCheckBox.checked = task.completed;
 
-    liCheckBox.addEventListener("change", (e) => {
+    liCheckBox.addEventListener('change', (e) => {
       TaskHelper.toggleStatus(task.index, TaskHelper.tasksList);
       LocalStorageHelper.updateEntryInLocalStorage(
         tasksKeyInLocalStorage,
-        TaskHelper.tasksList
+        TaskHelper.tasksList,
       );
 
-      const taskDescription =
-        e.target.parentNode.querySelector(".task-description");
+      const taskDescription = e.target.parentNode.querySelector('.task-description');
       if (task.completed) {
-        taskDescription.classList.add("description-check");
+        taskDescription.classList.add('description-check');
       } else {
-        taskDescription.classList.remove("description-check");
+        taskDescription.classList.remove('description-check');
       }
     });
 
-    const liText = document.createElement("input");
-    liText.type = "text";
+    const liText = document.createElement('input');
+    liText.type = 'text';
     liText.value = task.description;
-    liText.classList.add("task-description", "disabled");
-    liText.addEventListener("click", (e) => {
-      liText.classList.remove("disabled");
+    liText.classList.add('task-description', 'disabled');
+    liText.addEventListener('click', (e) => {
+      liText.classList.remove('disabled');
 
       e.target.parentNode.parentNode
-        .querySelector("#edit-btn")
-        .classList.add("d-none");
-      const deleteBtn =
-        e.target.parentNode.parentNode.querySelector("#delete-btn");
-      deleteBtn.classList.remove("d-none");
+        .querySelector('#edit-btn')
+        .classList.add('d-none');
+      const deleteBtn = e.target.parentNode.parentNode.querySelector('#delete-btn');
+      deleteBtn.classList.remove('d-none');
 
-      const taskDescription =
-        e.target.parentNode.querySelector(".task-description");
+      const taskDescription = e.target.parentNode.querySelector('.task-description');
       taskDescription.disabled = false;
       taskDescription.focus();
     });
-    liText.addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
+    liText.addEventListener('keyup', (e) => {
+      if (e.key === 'Enter') {
         TaskHelper.edit(task.index, e.target.value);
         LocalStorageHelper.updateEntryInLocalStorage(
           tasksKeyInLocalStorage,
-          TaskHelper.tasksList
+          TaskHelper.tasksList,
         );
         LoadContent.loadList();
         e.target.disabled = true;
@@ -63,34 +60,33 @@ export default class LoadContent {
       }
     });
     if (task.completed) {
-      liText.classList.add("description-check");
+      liText.classList.add('description-check');
     }
 
-    const editBtn = document.createElement("a");
-    editBtn.classList.add("fas", "fa-ellipsis-v", "task-link");
-    editBtn.id = "edit-btn";
+    const editBtn = document.createElement('a');
+    editBtn.classList.add('fas', 'fa-ellipsis-v', 'task-link');
+    editBtn.id = 'edit-btn';
 
-    editBtn.addEventListener("click", (e) => {
-      liText.classList.remove("disabled");
-      e.target.classList.add("d-none");
-      const deleteBtn = e.target.parentNode.querySelector("#delete-btn");
-      deleteBtn.classList.remove("d-none");
+    editBtn.addEventListener('click', (e) => {
+      liText.classList.remove('disabled');
+      e.target.classList.add('d-none');
+      const deleteBtn = e.target.parentNode.querySelector('#delete-btn');
+      deleteBtn.classList.remove('d-none');
 
-      const taskDescription =
-        e.target.parentNode.querySelector(".task-description");
+      const taskDescription = e.target.parentNode.querySelector('.task-description');
       taskDescription.disabled = false;
       taskDescription.focus();
     });
 
-    const deleteBtn = document.createElement("a");
-    deleteBtn.classList.add("far", "fa-trash-alt", "task-link", "d-none");
-    deleteBtn.id = "delete-btn";
+    const deleteBtn = document.createElement('a');
+    deleteBtn.classList.add('far', 'fa-trash-alt', 'task-link', 'd-none');
+    deleteBtn.id = 'delete-btn';
 
-    deleteBtn.addEventListener("click", () => {
+    deleteBtn.addEventListener('click', () => {
       TaskHelper.remove(task.index);
       LocalStorageHelper.updateEntryInLocalStorage(
         tasksKeyInLocalStorage,
-        TaskHelper.tasksList
+        TaskHelper.tasksList,
       );
       LoadContent.loadList();
     });
@@ -105,12 +101,11 @@ export default class LoadContent {
   }
 
   static loadList() {
-    const tasks =
-      LocalStorageHelper.retrieveFromLocalStorage(tasksKeyInLocalStorage) || [];
+    const tasks = LocalStorageHelper.retrieveFromLocalStorage(tasksKeyInLocalStorage) || [];
     TaskHelper.tasksList = tasks;
     TaskHelper.tasksList.sort((a, b) => a.index - b.index);
-    const tasksUL = document.querySelector("#tasks");
-    tasksUL.innerHTML = "";
+    const tasksUL = document.querySelector('#tasks');
+    tasksUL.innerHTML = '';
     TaskHelper.tasksList.forEach((task) => {
       tasksUL.appendChild(LoadContent.createLiForTask(task));
     });
@@ -120,30 +115,30 @@ export default class LoadContent {
 LoadContent.loadList();
 
 // add event listeners for input
-const taskInput = document.querySelector("#task-input");
-taskInput.addEventListener("keyup", (e) => {
-  if (e.key === "Enter" && e.target.value !== "") {
+const taskInput = document.querySelector('#task-input');
+taskInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter' && e.target.value !== '') {
     const task = new TaskHelper(TaskHelper.tasksList.length, e.target.value);
     TaskHelper.addNew(task);
     LocalStorageHelper.updateEntryInLocalStorage(
       tasksKeyInLocalStorage,
-      TaskHelper.tasksList
+      TaskHelper.tasksList,
     );
 
     LoadContent.loadList();
-    e.target.value = "";
+    e.target.value = '';
   }
 });
 
 // clear all completed tasks
 const clearAllCompletedTasksBtn = document.querySelector(
-  "#clear-all-completed"
+  '#clear-all-completed',
 );
-clearAllCompletedTasksBtn.addEventListener("click", () => {
+clearAllCompletedTasksBtn.addEventListener('click', () => {
   TaskHelper.removeAllCompleted();
   LocalStorageHelper.updateEntryInLocalStorage(
     tasksKeyInLocalStorage,
-    TaskHelper.tasksList
+    TaskHelper.tasksList,
   );
   LoadContent.loadList();
 });
